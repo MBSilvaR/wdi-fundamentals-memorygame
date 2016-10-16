6ba393dcdcc51225b2c6deba3e3c0189b6c39143
MarcelaSilvaRamirez <marcelabsr@me.com>
2016-10-16 09:40:30 -0400

    Update main.js

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,15 @@ var cardOne = "king";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "queen";
+var gameBoard = document.getElementByID("game-board");
+
+
+var createCards = function () {
+for (var x = 0; x < 4; x++){
+	var newDiv = document.createElement('div');
+	newDiv.id = 'game-card' + x;
+	newDiv.className = 'card';
+}
+};
+
+document.appendChild (gameBoard);
